8d5a6417bd7368577b95b4fcaa05a1a04ff26b6a
Joshua Peek <josh@joshpeek.com>
2016-12-09 22:45:33 -0800

    Add cop test

---
+++ test/test_application_record.rb
@@ -0,0 +1,37 @@
+require "rubocop/cop/github/application_record"
+require "minitest/autorun"
+
+class TestApplicationRecord < MiniTest::Test
+  def setup
+    config = RuboCop::Config.new
+    @cop = RuboCop::Cop::GitHub::ApplicationRecord.new(config)
+  end
+
+  def test_good_model
+    investigate(@cop, <<-RUBY)
+      class Repository < ApplicationRecord
+      end
+    RUBY
+
+    assert_empty @cop.offenses.map(&:message)
+  end
+
+  def test_bad_model
+    investigate(@cop, <<-RUBY)
+      class Repository < ActiveRecord::Base
+      end
+    RUBY
+
+    assert_equal 1, @cop.offenses.count
+    assert_equal "Models should subclass from ApplicationRecord", @cop.offenses.first.message
+  end
+
+  private
+
+  def investigate(cop, src)
+    processed_source = RuboCop::ProcessedSource.new(src, RUBY_VERSION.to_f)
+    commissioner = RuboCop::Cop::Commissioner.new([cop], [], raise_error: true)
+    commissioner.investigate(processed_source)
+    commissioner
+  end
+end
