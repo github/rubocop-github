3b172b18d9274729758e711da4fbca662b180557
Joshua Peek <josh@joshpeek.com>
2016-12-09 22:35:59 -0800

    Add ApplicationRecord cop

---
+++ lib/rubocop/cop/github/application_record.rb
@@ -0,0 +1,21 @@
+require "rubocop"
+
+module RuboCop
+  module Cop
+    module GitHub
+      class ApplicationRecord < Cop
+        MSG = "Models should subclass from ApplicationRecord"
+
+        ACTIVE_RECORD_BASE = s(:const, s(:const, nil, :ActiveRecord), :Base)
+
+        def on_class(node)
+          _name, superclass, _body = *node
+
+          if superclass == ACTIVE_RECORD_BASE
+            add_offense(superclass, :expression)
+          end
+        end
+      end
+    end
+  end
+end
