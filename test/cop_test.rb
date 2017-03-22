295ea2c9bc6520c7e3d5f160066f1e494351309f
Joshua Peek <josh@joshpeek.com>
2017-03-22 15:52:39 -0700

    Extract common cop test base

---
+++ test/cop_test.rb
@@ -0,0 +1,21 @@
+require "minitest"
+
+class CopTest < MiniTest::Test
+  def cop_class
+    raise NotImplementedError
+  end
+
+  attr_reader :cop
+
+  def setup
+    config = RuboCop::Config.new
+    @cop = cop_class.new(config)
+  end
+
+  def investigate(cop, src, filename = nil)
+    processed_source = RuboCop::ProcessedSource.new(src, RUBY_VERSION.to_f, filename)
+    commissioner = RuboCop::Cop::Commissioner.new([cop], [], raise_error: true)
+    commissioner.investigate(processed_source)
+    commissioner
+  end
+end
--- test/test_rails_application_record.rb
+++ test/test_rails_application_record.rb
@@ -1,48 +1,39 @@
 # frozen_string_literal: true
 
-require "rubocop/cop/github/rails_application_record"
+require "cop_test"
 require "minitest/autorun"
+require "rubocop/cop/github/rails_application_record"
 
-class TestRailsApplicationRecord < MiniTest::Test
-  def setup
-    config = RuboCop::Config.new
-    @cop = RuboCop::Cop::GitHub::RailsApplicationRecord.new(config)
+class TestRailsApplicationRecord < CopTest
+  def cop_class
+    RuboCop::Cop::GitHub::RailsApplicationRecord
   end
 
   def test_good_model
-    investigate(@cop, <<-RUBY)
+    investigate(cop, <<-RUBY)
       class Repository < ApplicationRecord
       end
     RUBY
 
-    assert_empty @cop.offenses.map(&:message)
+    assert_empty cop.offenses.map(&:message)
   end
 
   def test_application_record_model
-    investigate(@cop, <<-RUBY)
+    investigate(cop, <<-RUBY)
       class ApplicationRecord < ActiveRecord::Base
       end
     RUBY
 
-    assert_empty @cop.offenses.map(&:message)
+    assert_empty cop.offenses.map(&:message)
   end
 
   def test_bad_model
-    investigate(@cop, <<-RUBY)
+    investigate(cop, <<-RUBY)
       class Repository < ActiveRecord::Base
       end
     RUBY
 
-    assert_equal 1, @cop.offenses.count
-    assert_equal "Models should subclass from ApplicationRecord", @cop.offenses.first.message
-  end
-
-  private
-
-  def investigate(cop, src)
-    processed_source = RuboCop::ProcessedSource.new(src, RUBY_VERSION.to_f)
-    commissioner = RuboCop::Cop::Commissioner.new([cop], [], raise_error: true)
-    commissioner.investigate(processed_source)
-    commissioner
+    assert_equal 1, cop.offenses.count
+    assert_equal "Models should subclass from ApplicationRecord", cop.offenses.first.message
   end
 end
