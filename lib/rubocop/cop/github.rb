a21147e5827bd0c4747e407d321baf4414f1ff37
Joshua Peek <josh@joshpeek.com>
2017-03-22 16:23:36 -0700

    Add Rails controller/view render paths exist cop

--- lib/rubocop/cop/github.rb
+++ lib/rubocop/cop/github.rb
@@ -1,3 +1,5 @@
 require "rubocop/cop/github/rails_application_record"
+require "rubocop/cop/github/rails_controller_render_paths_exist"
 require "rubocop/cop/github/rails_render_inline"
 require "rubocop/cop/github/rails_render_object_collection"
+require "rubocop/cop/github/rails_view_render_paths_exist"
