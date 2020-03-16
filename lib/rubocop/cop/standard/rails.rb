8092374a30291579ec5e992885610b74fbe690c6
Garen J. Torikian <gjtorikian@users.noreply.github.com>
2020-03-16 22:41:07 +0000

    this one is dumb

--- lib/rubocop/cop/standard/rails.rb
+++ lib/rubocop/cop/standard/rails.rb
@@ -2,7 +2,6 @@
 
 require 'rubocop/cop/standard/rails/rails_application_record'
 require 'rubocop/cop/standard/rails/rails_controller_render_action_symbol'
-require 'rubocop/cop/standard/rails/rails_controller_render_literal'
 require 'rubocop/cop/standard/rails/rails_controller_render_paths_exist'
 require 'rubocop/cop/standard/rails/rails_controller_render_shorthand'
 require 'rubocop/cop/standard/rails/rails_render_inline'
