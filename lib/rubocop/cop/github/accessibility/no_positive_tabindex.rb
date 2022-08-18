7c2095e00876e214eddeb670e160b7c1e74af498
ydah <13041216+ydah@users.noreply.github.com>
2022-08-18 21:35:43 +0900

    Correct `InternalAffairs/NodeTypePredicate:`

--- lib/rubocop/cop/github/accessibility/no_positive_tabindex.rb
+++ lib/rubocop/cop/github/accessibility/no_positive_tabindex.rb
@@ -13,10 +13,10 @@ module RuboCop
             receiver, method_name, *args = *node
             if receiver.nil?
               args.select do |arg|
-                arg.type == :hash
+                arg.hash_type?
               end.each do |hash|
                 hash.each_pair do |key, value|
-                  next if key.type == :dsym
+                  next if key.dsym_type?
                   next unless key.respond_to?(:value)
                   if key.value == :tabindex && value.source.to_i > 0
                     add_offense(hash)
